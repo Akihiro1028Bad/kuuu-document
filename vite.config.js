76fd46783938b46ea63723f59216eb7efff182e2
tsu-tsu-aki <a_tsutsumi@fujiball.co.jp>
2025-03-12 09:13:11 +0900

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/kuuu-document/', // ここをGitHubリポジトリ名に変更
-});
